74375c12ea7f0ef74a6f7c5480108e0f5120037b
modernWebDev9 <susan0907miller@gmail.com>
2026-07-07 05:10:20 +0800

    fix:image load

--- roblox/asgi.py
+++ roblox/asgi.py
@@ -4,12 +4,70 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from people import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roblox.settings')
 
+# Get the Django ASGI application
+django_asgi_app = get_asgi_application()
+
+# Serve static files in development
+if settings.DEBUG:
+    from django.core.handlers.asgi import ASGIHandler
+    from django.http import HttpResponse
+    from django.contrib.staticfiles import finders
+    
+    class StaticFilesMiddleware:
+        def __init__(self, app):
+            self.app = app
+        
+        async def __call__(self, scope, receive, send):
+            if scope['type'] == 'http' and scope['path'].startswith('/static/'):
+                # Try to find the static file
+                file_path = scope['path'][8:]  # Remove '/static/'
+                found = finders.find(file_path)
+                if found:
+                    try:
+                        with open(found[1], 'rb') as f:
+                            content = f.read()
+                        await send({
+                            'type': 'http.response.start',
+                            'status': 200,
+                            'headers': [
+                                [b'content-type', self._get_content_type(found[1])],
+                            ],
+                        })
+                        await send({
+                            'type': 'http.response.body',
+                            'body': content,
+                        })
+                        return
+                    except:
+                        pass
+            await self.app(scope, receive, send)
+        
+        def _get_content_type(self, file_path):
+            if file_path.endswith('.css'):
+                return b'text/css'
+            elif file_path.endswith('.js'):
+                return b'application/javascript'
+            elif file_path.endswith('.png'):
+                return b'image/png'
+            elif file_path.endswith('.jpg') or file_path.endswith('.jpeg'):
+                return b'image/jpeg'
+            elif file_path.endswith('.webp'):
+                return b'image/webp'
+            elif file_path.endswith('.svg'):
+                return b'image/svg+xml'
+            else:
+                return b'application/octet-stream'
+    
+    django_asgi_app = StaticFilesMiddleware(django_asgi_app)
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter([
             path('ws/status/', consumers.StatusConsumer.as_asgi()),
--- roblox/settings.py
+++ roblox/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-no)gv)t_3@pmx(b=h$&j4#e%%rf0ox+0o&!b^ufnk3b*x$f%kp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'roblox.urls'
