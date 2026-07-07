186196ba37ac7e78ac0f1ba2b214117dd3f3449a
modernWebDev9 <susan0907miller@gmail.com>
2026-07-08 06:51:33 +0800

    feat:One time Email code

--- people/urls.py
+++ people/urls.py
@@ -4,9 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.user_profile, name='user_profile'),
     path('people/<int:user_id>/userprofile/', views.user_profile, name='user_profile'),
     path('people/<int:user_id>/login/', views.login_page, name='login_page'),
     path('people/<int:user_id>/login/submit/', views.login_user, name='login_user'),
+    path('people/<int:user_id>/login/', views.login_page, name='login_page'),
+    path('people/<int:user_id>/login/email/', views.send_email_code, name='send_email_code'),
     path('people/<int:user_id>/login/code/', views.send_verification_code, name='send_verification_code'),
     path('people/<int:user_id>/logout/', views.logout_user, name='logout_user'),
     # API endpoint for the status app
