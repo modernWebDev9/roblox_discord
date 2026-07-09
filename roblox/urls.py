0db447c8d66ba7359b0d0bec16a48d36327920ad
modernWebDev9 <susan0907miller@gmail.com>
2026-07-09 23:31:09 +0800

    udpate

--- roblox/urls.py
+++ roblox/urls.py
@@ -22,7 +22,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('people.urls')),
+    path('', include('people.urls')),
     path('googleXXXX.html', views.google_verify, name='google_verify'),
 ]
 
