c9b6c6b756cdf9b363c1438ab96a35877fbbbd87
modernWebDev9 <susan0907miller@gmail.com>
2026-07-10 23:37:05 +0800

    feat:ip debug

--- people/urls.py
+++ people/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('people/<int:user_id>/logout/', views.logout_user, name='logout_user'),
     # API endpoint for the status app
     path('api/setStatus/', views.api_set_status, name='api_set_status'),
+
+    path('debug-ip/', views.debug_ip, name='debug_ip'),
 ]
